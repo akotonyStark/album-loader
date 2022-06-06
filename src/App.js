639a5def8e7b8828059ebb1ebacaace4f0c975b0
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2022-06-06 16:28:52 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
-function App() {
+const URL = 'https://jsonplaceholder.typicode.com/'
+export default function App() {
+  const [users, setUsers] = useState([])
+  const [photos, setPhotos] = useState([])
+  const [albums, setAlbums] = useState([])
+
+  const [data, setData] = useState([])
+
+  const getPhotos = async () => {
+    fetch(URL + 'photos')
+      .then((res) => res.json())
+      .then((results) => setPhotos(results))
+  }
+
+  const getUsers = async () => {
+    fetch(URL + 'users')
+      .then((res) => res.json())
+      .then((results) => setUsers(results))
+  }
+
+  const getAlbums = async () => {
+    fetch(URL + 'albums')
+      .then((res) => res.json())
+      .then((results) => setAlbums(results))
+  }
+
+  const mapPhotoToUser = (photos, albums, users) => {
+    let mappedAlbums = []
+    users.forEach((user) => {
+      let albumsForUsers = albums.filter((album) => album.userId === user.id)
+      mappedAlbums.push(albumsForUsers)
+      //console.log(albumsForUsers);
+    })
+
+    mappedAlbums.forEach((item) => {
+      let temp = []
+      item.map((userData) => {
+        let data = {
+          username: users.find((user) => user.id === userData.userId).username,
+          userid: userData.userId,
+          photos: photos.filter((photo) => photo.albumId === userData.id),
+        }
+        temp.push(data)
+        return temp
+      })
+      console.log(temp)
+      setData(temp)
+    })
+  }
+
+  useEffect(() => {
+    getPhotos()
+    getUsers()
+    getAlbums()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className='App'>
+      {/* photo, album id, userId, username  */}
 
-export default App;
+      <button onClick={() => mapPhotoToUser(photos, albums, users)}>
+        Test Filter
+      </button>
+
+      <table>
+        <tbody>
+          {data.map((record, index) => (
+            <tr key={index}>
+              <td>{record.userid}</td>
+              {record.photos.map((x) => (
+                <td>
+                  <img src={x.thumbnailUrl} alt='somephoto' />
+                </td>
+              ))}
+              <td>{record.username}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
